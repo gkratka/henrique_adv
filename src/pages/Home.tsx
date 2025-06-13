44011923cda6a354834cf0e1654ac4842194b31d
gkratka <gkratka@Guilhermes-Air.home>
2025-06-12 17:29:59 -0700

    Set contact section background to light grey (bg-gray-50)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -283,7 +283,7 @@ const Home = () => {
       </section>
 
       {/* Contact Me Section */}
-      <section className="py-20 bg-white">
+      <section className="py-20 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid md:grid-cols-2 gap-12 items-start">
             {/* Contact Info Column */}
